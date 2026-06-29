1baeed57cc0c193e9a76475e0c7c748dab324038
pawanr0716 <pawanr0716@gmail.com>
2026-06-29 10:27:37 +0530

    git test

--- routers/company.py
+++ routers/company.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter,HTTPException,Depends,status
 from schemas.company import CompanyCreate,CompanyUpdate,CompanyResponse
-from models import company,job
 from models.company import Company
 from sqlalchemy.orm import Session
 from database import get_db,SessionLocal
@@ -17,17 +16,33 @@ def create_company(company:CompanyCreate, db:Session=Depends(get_db)):
 
 @router.get("/",status_code=status.HTTP_200_OK,response_model=list[CompanyResponse])
 def get_all_company(db:Session=Depends(get_db)):
-    pass
+    companies = db.query(Company).all()
+    return companies
 
 @router.get("/{company_id}",status_code=status.HTTP_200_OK,response_model=CompanyResponse)
 def get_company(company_id: int, db:Session=Depends(get_db)):
-    pass
+    company = db.query(Company).filter(Company.id == company_id).first()
+    if not company:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Company not found")
+    return company
 
 @router.put("/{company_id}",status_code=status.HTTP_200_OK)
 def update_company(company_id:int,company:CompanyUpdate, db:Session=Depends(get_db)):
-    pass
+    db_company = db.query(Company).filter(Companu.id == company_id).first()
+    if not db_company:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Company not found")
+    for key, value in company.dict().items():
+        setattr(db_company, key, value)
+    db.commit()
+    db.refresh(db_company)
+    return db_company
+    
 
 @router.delete("/{company_id}",status_code=status.HTTP_204_NO_CONTENT)
 def delete_company(company_id: int,db:Session=Depends(get_db)):
-    pass
+    db_company = db.query(Company).filter(Company.id == company_id).first()
+    if not db_company:
+        raise HTTPException(status_code=ststus.HTTP_404_NOT_FOUND, detail="Company not found")
+    db.delete(db_company)
+    db.commit()
     return {"message": "Company deleted successfully"}
