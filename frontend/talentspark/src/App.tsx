4db218853e42e79e93b28e7e585ec4ec8dc56b59
pawanr0716 <pawanr0716@gmail.com>
2026-06-30 12:38:25 +0530

    git test

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -1,20 +1,40 @@
-import Welcome from "./components/Welcome";
 import NavBar from "./components/NavBar";
+import Welcome from "./components/Welcome";
+import Footer from "./components/Footer";
 import CompanyCard from "./components/CompanyCard";
 import JobCard from "./components/JobCard";
-import Footer from "./components/Footer";
+import {useState,useEffect} from "react";
+import { getCompanies } from "./Services/CompanyService";
+import type { Company } from "./types/company";
+function App() {
+  const[loading,setLoading]=useState(true);
+  const[error,setError]=useState<Error|null>(null);
+  const[companies,setCompanies]=useState<Company[]>([]);
+  async function fetchCompanies(){
+    setLoading(true);
+    try{
+      const companies=await getCompanies();
+      setCompanies(companies);
+    } catch (err) {
+      setError(err as Error);
+    } finally {
+      setLoading(false);
+    }
+  }
 
-function App(){
-  return(
+  useEffect(() => {
+    fetchCompanies();
+  }, []);
+
+  return (
     <>
-    <NavBar/>
-    <Welcome/>
-    <br/>
-    <CompanyCard/>
-    <JobCard/>
-    <Footer/>
+      <NavBar />
+      <Welcome />
+      <CompanyCard />
+      <JobCard />
+      <Footer />
     </>
-  )
+  );
 }
-
-export default App
+  
+export default App;
---
+++ frontend/talentspark/src/types/job.ts
@@ -0,0 +1,8 @@
+interface job{
+    id:number;
+    Title:string;
+    description:string;
+    salary:string;
+    company_id:number;
+}
+export type {job}
